86df9cd728eb8c066f82ef2ccee9226923fa9c11
agfline <dev.agfline@posteo.net>
2018-07-28 18:09:26 +0200

    use common/debug.h macros instead of locals

--- LibAAF/LibCFB/LibCFB.c
+++ LibAAF/LibCFB/LibCFB.c
@@ -103,99 +103,11 @@
 
 #include "LibCFB.h"
 #include "CFBTypes.h"
+#include "../common/debug.h"
 
 
 
 
-
-
-/**
- *	@name Status printing macros
- *	@{
- */
-
-/**
- *	Displays a warning with its associated source filename,
- *	function name, line number and a custom message.
- *
- *	The `if ( 1 )` allows to expand the macro inside an
- *	if statement with or without brackets.
- *
- *	This macro doesn't change the execution flow.
- */
-
-#define _warning( ... )                              \
-	if ( 1 )                                         \
-	{                                                \
-	    fprintf( stderr, "WARNING %s:%d in %s() : ", \
-	        __FILE__,                                \
-	        __LINE__,                                \
-	        __func__ );                              \
-	    fprintf( stderr, __VA_ARGS__ );              \
-	}
-
-
-
-
-/**
- *	Displays an error with its associated source filename,
- *	function name, line number and a custom message.
- *
- *	The `if ( 1 )` allows to expand the macro inside an
- *	if statement with or without brackets.
- *
- *	This macro doesn't change the execution flow, however
- *	the user should interrupt the function right after
- *	calling _error().
- */
-
-#define _error( ... )                                \
-	if ( 1 )                                         \
-	{                                                \
-	    fprintf( stderr, "__ERROR %s:%d in %s() : ", \
-	        __FILE__,                                \
-	        __LINE__,                                \
-	        __func__ );                              \
-	    fprintf( stderr, __VA_ARGS__ );              \
-	}
-
-
-
-
-/**
- *	Displays an error with its associated source filename,
- *	function name, line number and a custom message.
- *
- *	The `if ( 1 )` allows to expand the macro inside an
- *	if statement with or without brackets.
- *
- *	This macro interrupts the execution and exits the program.
- */
-/*
-#define _fatal( ... )                                \
-	if ( 1 )                                         \
-	{                                                \
-	    fprintf( stderr, "__FATAL %s:%d in %s() : ", \
-	        __FILE__,                                \
-	        __LINE__,                                \
-	        __func__ );                              \
-	    fprintf( stderr, __VA_ARGS__ );              \
-	    exit( 1 );                                   \
-	}
-*/
-/**
- *	@}
- */
-
-
-
-
-
-
-
-
-
-/* Static Functions declaration */
 
 
 
@@ -211,7 +123,6 @@ static int cfb_is_valid( CFB_Data *cfbd );
 
 
 
-
 static int cfb_retrieveFileHeader( CFB_Data *cfbd );
 
 static int cfb_retrieveDiFAT( CFB_Data *cfbd );
@@ -224,13 +135,13 @@ static int cfb_retrieveNodes( CFB_Data *cfbd );
 
 
 
-
 static cfbSID_t getNodeCount( CFB_Data *cfbd );
 
 
 
-
 static cfbSID_t cfb_getIDByNode( CFB_Data *cfbd, cfbNode *node );
+
+
 
 
 
@@ -321,10 +232,6 @@ int cfb_load_file( CFB_Data *cfbd, const char * file )
 	{
 		return -1;
 	}
-
-	/*
-	 *	Ensures we have a valid CFB file.
-	 */
 
 	if ( cfb_is_valid( cfbd ) == 0 )
 	{
@@ -457,6 +364,7 @@ static int cfb_is_valid( CFB_Data *cfbd )
 
 	if ( cfbd->file_sz < sizeof(struct StructuredStorageHeader) )
 	{
+		_error( "Not a valid Compound File : File size is lower than header.\n" );
 		return 0;
 	}
 
@@ -467,7 +375,7 @@ static int cfb_is_valid( CFB_Data *cfbd )
 
 	if ( signature != 0xe11ab1a1e011cfd0 )
 	{
-		_error( "Not a valid Compound File.\n" );
+		_error( "Not a valid Compound File : Wrong signature.\n" );
 		return 0;
 	}
 
