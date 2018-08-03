fa934499a4164be931435810788a0d19b109c9a7
agfline <dev.agfline@posteo.net>
2018-08-03 17:34:21 +0200

    cleaning

--- LibAAF/AAFCore/AAFUtils.h
+++ LibAAF/AAFCore/AAFUtils.h
@@ -83,7 +83,5 @@
 
 size_t utf16toa( char *astr, uint16_t alen, uint16_t *wstr, uint16_t wlen );
 
-// void   printObjectProperties( AAF_Data *aafd, aafObject *Obj );
-
 
 #endif // ! __AAFUtils_h__
