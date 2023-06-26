93e6f54faed29639946366d8caa69dec37c623c2
agfline <dev.agfline@posteo.net>
2023-06-26 21:55:52 +0200

    Change license to GPL v2

--- include/libaaf/AAFCore.h
+++ include/libaaf/AAFCore.h
@@ -1,24 +1,25 @@
+/*
+ * Copyright (C) 2017-2023 Adrien Gesta-Fline
+ *
+ * This file is part of libAAF.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef __AAFCore_h__
 #define __AAFCore_h__
-
-/*
- *	This file is part of LibAAF.
- *
- *	Copyright (c) 2017 Adrien Gesta-Fline
- *
- *	LibAAF is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Affero General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	any later version.
- *
- *	LibAAF is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Affero General Public License for more details.
- *
- *	You should have received a copy of the GNU Affero General Public License
- *	along with LibAAF. If not, see <http://www.gnu.org/licenses/>.
- */
 
 /**
  *	@file AAFCore/AAFCore.h
--- tools/AAFInfo.c
+++ tools/AAFInfo.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2017-2023 Adrien Gesta-Fline
+ *
+ * This file is part of libAAF.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -147,9 +167,6 @@ void showHelp()
 {
 	printf(
 		"\n"
-		" AAFInfo 0.1\n"
-		"\n"
-		"\n"
 		" CFB Analysis :\n"
 		"\n"
 		"   --cfb-header                   Displays the CFB Header.\n"
@@ -224,6 +241,9 @@ int main( int argc, char *argv[] )
 	wchar_t *trace_class = NULL;
 
 	int cmd = 0;
+
+
+	printf( "\nAAFInfo\nlibAAF %s\n\n", LIBAAF_VERSION );
 
 
 	static struct option long_options[] = {
