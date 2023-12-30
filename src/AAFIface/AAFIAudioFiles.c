agfline/LibAAF

commit 58fb9397733840531626c5ce5c95218dff8488ae

get rid of useless RIFFParser error when parsing AudioSummary object

src/AAFIface/AAFIAudioFiles.c
@@ -479,7 +479,7 @@ int parse_audio_summary( AAF_Iface *aafi, aafiAudioEssence *audioEssence )
     // dump_hex( audioEssence->summary->val, audioEssence->summary->len );
 
 
-    rc = riff_parseAudioFile( &RIFFAudioFile, &embeddedAudioDataReaderCallback, audioEssence->summary->val, &audioEssence->summary->len, aafi, aafi->dbg );
+    rc = riff_parseAudioFile( &RIFFAudioFile, RIFF_PARSE_ONLY_HEADER, &embeddedAudioDataReaderCallback, audioEssence->summary->val, &audioEssence->summary->len, aafi, aafi->dbg );
 
     if ( rc < 0 ) {
       warning( "TODO: Could not parse embedded essence summary. Should try essence data stream ?" );
@@ -521,7 +521,7 @@ int parse_audio_summary( AAF_Iface *aafi, aafiAudioEssence *audioEssence )
     }
 
 
-    rc = riff_parseAudioFile( &RIFFAudioFile, &externalAudioDataReaderCallback, fp, externalFilePath, aafi, aafi->dbg );
+    rc = riff_parseAudioFile( &RIFFAudioFile, RIFF_PARSE_ONLY_HEADER, &externalAudioDataReaderCallback, fp, externalFilePath, aafi, aafi->dbg );
 
     if ( rc < 0 ) {
       error( "TODO IF MP3 ? Failed parsing external essence file : %s", externalFilePath );

src/AAFIface/RIFFParser.c
@@ -117,7 +117,7 @@ int riff_writeWavFileHeader( FILE *fp, struct wavFmtChunk *wavFmt, struct wavBex
 
 
 
-int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, size_t (*readerCallback)(unsigned char *, size_t, size_t, void*, void*, void*), void *user1, void *user2, void *user3, struct dbg *dbg ) {
+int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, enum RIFF_PARSER_FLAGS flags, size_t (*readerCallback)(unsigned char *, size_t, size_t, void*, void*, void*), void *user1, void *user2, void *user3, struct dbg *dbg ) {
 
   struct riffChunk chunk;
   struct riffHeaderChunk riff;
@@ -202,6 +202,10 @@ int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, size_t (*readerCal
         RIFFAudioFile->channels   = wavFmtChunk.channels;
         RIFFAudioFile->sampleSize = wavFmtChunk.bits_per_sample;
         RIFFAudioFile->sampleRate = wavFmtChunk.samples_per_sec;
+
+				if ( flags & RIFF_PARSE_ONLY_HEADER ) {
+					return 0;
+				}
       }
       else
       if ( chunk.ckid[0] == 'd' &&
@@ -229,6 +233,10 @@ int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, size_t (*readerCal
         RIFFAudioFile->sampleSize = BE2LE16(aiffCOMMChunk.sampleSize);
         RIFFAudioFile->sampleRate = beExtended2leUint32(aiffCOMMChunk.sampleRate);
         RIFFAudioFile->duration   = BE2LE32(aiffCOMMChunk.numSampleFrames);
+
+				if ( flags & RIFF_PARSE_ONLY_HEADER ) {
+					return 0;
+				}
       }
       // else
       // if ( chunk.ckid[0] == 'S' &&

src/AAFIface/RIFFParser.h
@@ -33,6 +33,10 @@
 #endif
 
 
+enum RIFF_PARSER_FLAGS {
+  RIFF_PARSE_ONLY_HEADER = (1 << 0),
+};
+
 struct riffContext {
   void *user;
   void DEBUG_FUNCTION_POINTER;
@@ -132,7 +136,7 @@ PACK(struct aiffCOMMChunk {
 
 
 
-int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, size_t (*readerCallback)(unsigned char *, size_t, size_t, void*, void*, void*), void *user1, void *user2, void *user3, struct dbg *dbg );
+int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, enum RIFF_PARSER_FLAGS flags, size_t (*readerCallback)(unsigned char *, size_t, size_t, void*, void*, void*), void *user1, void *user2, void *user3, struct dbg *dbg );
 
 int riff_writeWavFileHeader( FILE *fp, struct wavFmtChunk *wavFmt, struct wavBextChunk *wavBext, uint32_t audioDataSize, struct dbg *dbg );
 

commit 7faa2179c88800ee5bcbcded9bd171270b1b58e6

bug fix: missing audio duration when parsing external files
bug was introduced in 58fb939 with RIFF_PARSE_ONLY_HEADER. This flag is 
important when parsing internal summary, but it prevented from parsing 
data chunk of external audio files.

src/AAFIface/AAFIAudioFiles.c
@@ -596,7 +596,7 @@ int aafi_parse_audio_summary( AAF_Iface *aafi, aafiAudioEssence *audioEssence )
 			}
 
 
-			rc = riff_parseAudioFile( &RIFFAudioFile, RIFF_PARSE_ONLY_HEADER, &externalAudioDataReaderCallback, fp, externalFilePath, aafi, aafi->dbg );
+			rc = riff_parseAudioFile( &RIFFAudioFile, 0, &externalAudioDataReaderCallback, fp, externalFilePath, aafi, aafi->dbg );
 
 			if ( rc < 0 ) {
 				error( "Failed parsing external audio essence file : %s", externalFilePath );

src/AAFIface/RIFFParser.c
@@ -188,6 +188,12 @@ int riff_parseAudioFile( struct RIFFAudioFile *RIFFAudioFile, enum RIFF_PARSER_F
 
 		if ( !be ) { /* WAVE */
 
+			// debug( "Got chunck %c%c%c%c",
+			// 	chunk.ckid[0],
+			// 	chunk.ckid[1],
+			// 	chunk.ckid[2],
+			// 	chunk.ckid[3] );
+
 			if ( chunk.ckid[0] == 'f' &&
 			     chunk.ckid[1] == 'm' &&
 			     chunk.ckid[2] == 't' &&