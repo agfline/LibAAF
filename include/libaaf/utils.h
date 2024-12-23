c4b15918cf84dde3180f0589feca55485e5cce80
agfline <dev.agfline@posteo.net>
2024-12-23 07:50:19 +0100

    add laaf_util_fopen_utf8()

--- include/libaaf/utils.h
+++ include/libaaf/utils.h
@@ -91,6 +91,8 @@ char * laaf_util_windows_utf16toutf8( const wchar_t *wstr );
 
 int laaf_util_file_exists( const char *filepath );
 
+FILE * laaf_util_fopen_utf8( const char *filepath, const char *mode );
+
 char * laaf_util_clean_filename( char *filename );
 
 int laaf_util_is_fileext( const char *filepath, const char *ext );
